7888f42b8fe17badedfdd4b0eef151186782835f
RIKI SUPRIYATNA <rickysupriyatna@mhs.pelitabangsa.ac.id>
2023-01-01 00:37:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugasqrscaner21c2/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.tugasqrscaner21c2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // objek
+    private Button buttonScan;
+    private TextView textViewName, textViewClass, textViewID;
+
+    // QRCode Scanner
+    private IntentIntegrator qrscan;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // View
+        buttonScan = (Button) findViewById(R.id.buttonScan);
+        textViewName = (TextView) findViewById(R.id.textViewNama);
+        textViewClass = (TextView) findViewById(R.id.textViewKelas);
+        textViewID = (TextView) findViewById(R.id.textViewNIM);
+
+        qrscan = new IntentIntegrator(this);
+
+        buttonScan.setOnClickListener(this);
+
+// Meminta izin untuk mengakses fitur panggilan telepon
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CALL_PHONE}, 1);
+        }
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Not Scanned", Toast.LENGTH_LONG).show();
+            }
+            // JSON
+            try {
+                JSONObject jsonObject = new JSONObject(result.getContents());
+                textViewName.setText(jsonObject.getString("nama"));
+                textViewClass.setText(jsonObject.getString("kelas"));
+                textViewID.setText(jsonObject.getString("nim"));
+            } catch (JSONException e) {
+                // WEBVIEW
+                if (Patterns.WEB_URL.matcher(result.getContents()).matches()) {
+                    Intent visitUrl = new Intent(Intent.ACTION_VIEW, Uri.parse(result.getContents()));
+                    startActivity(visitUrl);
+                } else {
+                    // DIAL UP, NOMOR TELEPON
+                    try {
+                        Intent intent2 = new Intent(Intent.ACTION_CALL, Uri.parse(result.getContents()));
+                        startActivity(intent2);
+                    } catch (Exception e2) {
+                        Toast.makeText(this, "Not Scanned", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        qrscan.initiateScan();
+    }
+}
